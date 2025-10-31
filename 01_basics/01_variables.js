ba975ceed1e08e3974d50cea83c2a0926728eebc
ADYant17 <119745331+ADYant17@users.noreply.github.com>
2025-10-31 14:56:31 +0000

    this is for variables knowledge

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const your_id = 11223
+let sec_mail = "hcdwipro@gmail.com"
+var sec_password = 559911
+accountcity = "Delhi"
+let sec_state ;
+
+//your_id = 5 is not allowed 
+/*
+   Prefer not to use Var because of issue 
+   in block scope and functional scope 
+*/
+
+sec_mail = "yourslove@gmail.com"
+sec_password = 123456789
+accountcity = "mumbai"
+
+console.table([your_id,sec_mail,sec_password,accountcity,sec_state])

7dab71a70040c23b8afa88a5441139f089d4dd0a
ADYant17 <119745331+ADYant17@users.noreply.github.com>
2025-11-08 10:55:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//datatypes
+/*
+ number
+ bigint 
+ string => ""
+ boolean => ? true/false
+ null => standalone value
+ undefined => it is undefined type 
+ symbol
+   
+ //object  , this is an object type 
+*/
+let age = 14
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
